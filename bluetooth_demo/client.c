904773d3bf97f228ee546e7d7ebfb85f1567bf4a
Ernst Schwaiger <ernst.schwaiger@chello.at>
2025-09-13 15:11:12 +0200

    mixed up client and server code

--- bluetooth_demo/client.c
+++ bluetooth_demo/client.c
@@ -5,43 +5,44 @@
 #include <bluetooth/l2cap.h>
 
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-    struct sockaddr_l2 loc_addr = { 0 }, rem_addr = { 0 };
-    char buf[1024] = { 0 };
-    int s, client, bytes_read;
-    socklen_t opt = sizeof(rem_addr);
+    struct sockaddr_l2 addr = { 0 };
+    int s, status;
+    char dest[18] = "DC:A6:32:BB:79:EF";
 
-    // allocate socket
-    s = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
-
-    // bind socket to port 0x1001 of the first available
-    // bluetooth adapter
-    loc_addr.l2_family = AF_BLUETOOTH;
-    loc_addr.l2_bdaddr = *BDADDR_ANY;
-    loc_addr.l2_psm = htobs(0x1001);
-
-    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
-
-    // put socket into listening mode
-    listen(s, 1);
-
-    // accept one connection
-    client = accept(s, (struct sockaddr *)&rem_addr, &opt);
-
-    ba2str( &rem_addr.l2_bdaddr, buf );
-    fprintf(stderr, "accepted connection from %s\n", buf);
-
-    memset(buf, 0, sizeof(buf));
-
-    // read data from the client
-    bytes_read = read(client, buf, sizeof(buf));
-    if( bytes_read > 0 ) {
-        printf("received [%s]\n", buf);
+    if(argc < 2)
+    {
+        fprintf(stderr, "usage: %s <bt_addr>\n", argv[0]);
+        exit(2);
     }
 
-    // close connection
-    close(client);
+    strncpy(dest, argv[1], 18);
+
+    // allocate a socket
+    s = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
+
+    // set the connection parameters (who to connect to)
+    addr.l2_family = AF_BLUETOOTH;
+    addr.l2_psm = htobs(0x1001);
+    str2ba( dest, &addr.l2_bdaddr );
+
+    // connect to server
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+    // send a message
+    if( status == 0 )
+    {
+        char *message = "hallihallo!";
+        status = write(s, message, strlen(message));
+    }
+
+    if( status < 0 )
+    {
+        perror("uh oh");
+    }
+
     close(s);
 }
