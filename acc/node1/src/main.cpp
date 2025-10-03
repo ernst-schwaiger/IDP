dd5cd44209e6e65a0cf10deb7ffe250cd39f03dc
Ernst Schwaiger <ernst.schwaiger@chello.at>
2025-10-03 15:42:33 +0200

    Node 1 only sends measurements

--- acc/node1/src/main.cpp
+++ acc/node1/src/main.cpp
@@ -14,7 +14,38 @@ using namespace acc;
 // global var, written by the sensor thread, read by the communication thread
 volatile uint16_t gCurrentDistanceReading = 0xffff;
 
-void *sensorThread(void *arg)
+static unsigned long getTimestampMs()
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+
+    long ms = ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+    printf("Current time in ms: %ld\n", ms);
+    return 0;
+}
+
+static uint32_t getTimestampMsSinceBaseline(long baselineMs)
+{
+    long currentTimeMs = getTimestampMs();
+    return static_cast<uint32_t>(currentTimeMs - baselineMs);
+}
+
+static uint16_t getCurrentDistanceReading(pthread_mutex_t *pLock)
+{
+    pthread_mutex_lock(pLock);
+    uint16_t currentReading = gCurrentDistanceReading;
+    pthread_mutex_unlock(pLock);
+    return currentReading;
+}
+
+static void setCurrentDistanceReading(pthread_mutex_t *pLock, uint16_t value)
+{
+    pthread_mutex_lock(pLock);
+    gCurrentDistanceReading = value;
+    pthread_mutex_unlock(pLock);
+}
+
+static void *sensorThread(void *arg)
 {
     pthread_mutex_t *pLock = reinterpret_cast<pthread_mutex_t *>(arg);
 
@@ -35,10 +66,7 @@ void *sensorThread(void *arg)
         uint16_t nextReadingVal = static_cast<uint16_t>(floor(d));
         
         // Critical section start, take care that no exception can be thrown in it!
-        pthread_mutex_lock(pLock);
-        gCurrentDistanceReading = nextReadingVal;
-        pthread_mutex_unlock(pLock);
-        // Critical section end
+        setCurrentDistanceReading(pLock, nextReadingVal);
         
         // Sleep 2.5ms
         usleep(2'500);
@@ -52,49 +80,35 @@ static void commLoop(acc::BTListenSocket &listenSocket, pthread_mutex_t *pLock)
 {
     uint32_t numTxFails = 0;
     uint32_t numTxSuccess = 0;
-    uint32_t numRxFails = 0;
-    uint32_t numRxSuccess = 0;
-
     acc::BTConnection conn(&listenSocket);
-    array<uint8_t, MAX_MSG_LEN> rxBuf = { 0x00 };
-    uint8_t msgType = 0x00;
     
     // Set up session key
     conn.keyExchangeServer();
+    cout << "Connection to client established.\n";
+
+    long baselineMs = getTimestampMs();
 
     while (1) // we leave this one only via failed send/receive operations
     {
-        uint32_t counter = 0;
-        ssize_t bytes_received = conn.receiveWithCounterAndMAC(msgType, rxBuf, counter, false);
-        if (bytes_received < 0)
+        uint16_t currentDistanceReading = getCurrentDistanceReading(pLock);
+        uint8_t readingToTransmit[sizeof(currentDistanceReading)];
+        readingToTransmit[0] = currentDistanceReading >> 8;
+        readingToTransmit[1] = currentDistanceReading & 0xff;
+
+        if (conn.sendWithCounterAndMAC(0x02, {readingToTransmit, sizeof(readingToTransmit)}, getTimestampMsSinceBaseline(baselineMs)) < 0)
         {
-            numRxFails++;
+            numTxFails++;
         }
         else
         {
-            numRxSuccess++;
-            uint8_t readingToTransmit[sizeof(gCurrentDistanceReading)];
-            pthread_mutex_lock(pLock);
-            readingToTransmit[0] = gCurrentDistanceReading >> 8;
-            readingToTransmit[1] = gCurrentDistanceReading & 0xff;
-            pthread_mutex_unlock(pLock);
-
-            if (conn.sendWithCounterAndMAC(0x02, {readingToTransmit, sizeof(readingToTransmit)}, counter) < 0)
-            {
-                numTxFails++;
-            }
-            else
-            {
-                numTxSuccess++;
-            }
-        }
+            numTxSuccess++;
+        }        
 
         // Sleep 2.5ms
         usleep(2'500);
     }
 
-    cout << "Tx Fails: " << numTxFails << ", Rx Fails: " << numRxFails << "\n";
-    cout << "Tx Success: " << numTxSuccess << ", Rx Success: " << numRxSuccess << "\n";    
+    cout << "Tx Fails: " << numTxFails << ", Tx Success: " << numTxSuccess << "\n";
 }
 
 int main()
