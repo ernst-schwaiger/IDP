dc973970bea9f7de1fd515df7787a4413ad6e82f
stefan ohnewith <stefan.ohnewith@gmail.com>
2025-12-03 20:13:26 +0100

    remove files

--- acc/release/node1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stefan/IDP/acc/node1
-# Build directory: /home/stefan/IDP/acc/release/node1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
