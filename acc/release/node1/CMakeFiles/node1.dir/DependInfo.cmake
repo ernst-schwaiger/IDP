98891122c1c2bef913094bd9fd865721b35f98c5
stefan ohnewith <stefan.ohnewith@gmail.com>
2025-12-03 20:04:10 +0100

    clamp weg

---
+++ acc/release/node1/CMakeFiles/node1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/stefan/IDP/acc/node1/src/CommThread.cpp" "node1/CMakeFiles/node1.dir/src/CommThread.cpp.o" "gcc" "node1/CMakeFiles/node1.dir/src/CommThread.cpp.o.d"
+  "/home/stefan/IDP/acc/node1/src/Sensor.cpp" "node1/CMakeFiles/node1.dir/src/Sensor.cpp.o" "gcc" "node1/CMakeFiles/node1.dir/src/Sensor.cpp.o.d"
+  "/home/stefan/IDP/acc/node1/src/SensorThread.cpp" "node1/CMakeFiles/node1.dir/src/SensorThread.cpp.o" "gcc" "node1/CMakeFiles/node1.dir/src/SensorThread.cpp.o.d"
+  "/home/stefan/IDP/acc/node1/src/main.cpp" "node1/CMakeFiles/node1.dir/src/main.cpp.o" "gcc" "node1/CMakeFiles/node1.dir/src/main.cpp.o.d"
+  "" "node1/node1" "gcc" "node1/CMakeFiles/node1.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ acc/release/node1/CMakeFiles/node1.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node1.dir/link.d"
+  "CMakeFiles/node1.dir/src/CommThread.cpp.o"
+  "CMakeFiles/node1.dir/src/CommThread.cpp.o.d"
+  "CMakeFiles/node1.dir/src/Sensor.cpp.o"
+  "CMakeFiles/node1.dir/src/Sensor.cpp.o.d"
+  "CMakeFiles/node1.dir/src/SensorThread.cpp.o"
+  "CMakeFiles/node1.dir/src/SensorThread.cpp.o.d"
+  "CMakeFiles/node1.dir/src/main.cpp.o"
+  "CMakeFiles/node1.dir/src/main.cpp.o.d"
+  "node1"
+  "node1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
